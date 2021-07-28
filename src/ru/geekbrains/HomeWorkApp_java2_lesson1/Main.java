4710fe17bb8a1cbc9b02d787ae5d7bfcd46c7c2c
Mercesletifer47 <art-matv98@yandex.ru>
2021-07-28 19:22:14 +0300

    Actual dev

--- src/ru/geekbrains/HomeWorkApp_java2_lesson1/Main.java
+++ src/ru/geekbrains/HomeWorkApp_java2_lesson1/Main.java
@@ -4,31 +4,31 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] words = new String[]{"Ауди","Мерседес","БМВ",
-                "Инфинити","Лексус","Порше",
-                "Ауди","Мерседес","БМВ",
-                "Инфинити","Лексус","Порше","Ауди","Мерседес","БМВ",
-                "Инфинити","Лексус","Порше","Ауди","Мерседес","БМВ",
-                "Инфинити","Лексус","Порше","Ауди","Мерседес","БМВ",
-                "Инфинити","Лексус","Порше","Ауди","Мерседес","БМВ",
-                "Инфинити","Лексус","Порше"};
+        String[] words = {"Ауди", "Мерседес", "БМВ",
+                "Инфинити", "Лексус", "Порше",
+                "Ауди", "Мерседес", "БМВ",
+                "Инфинити", "Лексус", "Порше", "Ауди", "Мерседес", "БМВ",
+                "Инфинити", "Лексус", "Порше", "Ауди", "Мерседес", "БМВ",
+                "Инфинити", "Лексус", "Порше", "Ауди", "Мерседес", "БМВ",
+                "Инфинити", "Лексус", "Порше", "Ауди", "Мерседес", "БМВ",
+                "Инфинити", "Лексус", "Порше"};
 
-        Set<String> setWords = new HashSet<>();
-        Collections.addAll(setWords, words);
-
-        for(String word : setWords) {
-            System.out.println(word);
+        Map<String, Integer> hmap = new HashMap<>();
+        for (String x : words) {
+            hmap.put(x, hmap.getOrDefault(x, 0) + 1);
         }
+        System.out.println(hmap);
+
 
         Map<String, String> phonebook = new Phonebook();
         ((Phonebook) phonebook).add("Матвеев", "8996999999");
         ((Phonebook) phonebook).add("Геворгян", "896675852369");
-        ((Phonebook) phonebook).add("Иванов", "91766582459");
+        ((Phonebook) phonebook).add("Матвеев", "91766582459");
 
         for (Map.Entry<String, String> entry : phonebook.entrySet()) {
             System.out.println("Name =  " + entry.getKey() + " Телефон = " + entry.getValue());
         }
-        System.out.println(phonebook.get("Иванов"));
+        System.out.println(phonebook.get("Матвеев"));
     }
 }
 
